05d7fa4e70e3344b638ddd7153e98a6a1d052f78
Armen Matevosyan <armen@Armens-Mac-Pro.local>
2014-07-26 02:48:19 -0700

    Initial Commit

---
+++ Pickers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pickers
+//
+//  Created by Armen Matevosyan on 7/26/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
